40cb90208af33a2f4c03812ee16bdea8ab1d4987
Yan Soares Couto <yancouto@gmail.com>
2024-08-21 21:49:49 -0300

    Basic herbivore logic

--- src/ep1/agents.py
+++ src/ep1/agents.py
@@ -1,16 +1,23 @@
-from arcade import Sprite
+from arcade import Sprite, SpriteSolidColor
 import arcade, arcade.color
-from constants import OBJ_SIZE, DT
+from constants import OBJ_SIZE, DT, SCREEN_HEIGHT, SCREEN_WIDTH
 import random
 from util import rand
 from dataclasses import dataclass
+from typing import cast
 
-class AgentWithHealth(Sprite):
+class Agent(Sprite):
+    map: "Map"
+    
+    def __init__(self, map, left, top, *args, **kwargs):
+        super().__init__(*args, **kwargs, hit_box_algorithm="Simple")
+        self.left = left
+        self.top = top
+        self.map = map
+
+class AgentWithHealth(Agent):
     health = 100.0
     health_regen = 0.0
-    
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs, hit_box_algorithm="None")
 
     def draw(self, **kwargs):
         super().draw(**kwargs)
@@ -25,19 +32,23 @@ class AgentWithHealth(Sprite):
     def is_dead(self) -> bool:
         return self.health <= 0
     
+    def remove_health(self, damage: float) -> float:
+        health_drop = min(self.health, damage)
+        self.health -= health_drop
+        return health_drop
+    
     def update(self):
         super().update()
-        self.health = min(100, self.health + self.health_regen * DT)
         if self.health <= 0:
             print("%s died" % self)
             self.kill()
+        else:
+            self.health = min(100, self.health + self.health_regen * DT)
 
 class Grass(AgentWithHealth):
     health_regen = 5.0
-    def __init__(self, left, top):
-        super().__init__(":resources:images/tiles/cactus.png", scale=OBJ_SIZE / 128)
-        self.left = left
-        self.top = top
+    def __init__(self, *args):
+        super().__init__(*args, ":resources:images/tiles/cactus.png", scale=OBJ_SIZE / 128)
         assert(self.width == OBJ_SIZE and self.height == OBJ_SIZE)
 
 class AgentWithHunger(AgentWithHealth):
@@ -62,12 +73,16 @@ class AgentWithHunger(AgentWithHealth):
         hunger_bar_x = self.left + hunger_bar_width / 2
         hunger_bar_y = self.top - hunger_bar_height / 2 - 5
         arcade.draw_rectangle_filled(hunger_bar_x, hunger_bar_y, hunger_bar_width, hunger_bar_height, arcade.color.RED)
+    
+    def remove_hunger(self, amount: float):
+        self.hunger = max(0, self.hunger - amount)
 
     @property
     def is_hungry(self) -> bool:
         return self.hunger >= 50.0
 
 class Herbivore(AgentWithHunger):
+    HEALTH_TO_HUNGER: float = 1.2
     health_regen = 1.0
     class HState: pass
     @dataclass
@@ -84,12 +99,12 @@ class Herbivore(AgentWithHunger):
         target: Grass
     state: HState
     
-    def __init__(self, left, top):
-        super().__init__(":resources:images/enemies/wormPink.png", scale=OBJ_SIZE / 128)
+    def __init__(self, *args):
+        super().__init__(*args, ":resources:images/enemies/wormPink.png", scale=OBJ_SIZE / 128)
         self.state = Herbivore.Idle.random(3)
-        self.left = left
-        self.top = top
         self.idle_speed: float = rand(0.1, 0.5)
+        self.chase_speed: float = rand(1.0, 2.0)
+        self.eat_speed: float = rand(15, 20)
         assert(self.width == OBJ_SIZE and self.height == OBJ_SIZE)
     
     def update(self):
@@ -98,8 +113,9 @@ class Herbivore(AgentWithHunger):
             case Herbivore.Idle(_) as state:
                 state.time_to_move -= DT
                 if self.is_hungry:
-                    #self.state = Herbivore.ChasingFood(find someone)
-                    pass
+                    closest = arcade.get_closest_sprite(self, self.map.scene.get_sprite_list("grass"))
+                    if closest is not None and isinstance(closest[0], Grass):
+                        self.state = Herbivore.ChasingFood(closest[0])
                 elif state.time_to_move <= 0:
                     if random.random() < 0.3:
                         self.velocity = [0, 0]
@@ -109,15 +125,56 @@ class Herbivore(AgentWithHunger):
             case Herbivore.ChasingFood(target):
                 if target.is_dead:
                     self.state = Herbivore.Idle.random(1)
+                elif self.collides_with_sprite(target):
+                    self.velocity = [0, 0]
+                    self.state = Herbivore.Eating(target)
                 else:
-                    # Chase
-                    pass
+                    self.angle = 0
+                    self.velocity = [0, 0]
+                    self.face_point(target.position)
+                    self.angle += 90
+                    self.forward(self.chase_speed)
             case Herbivore.Eating(target):
                 if target.is_dead:
                     self.state = Herbivore.Idle.random(1)
                 else:
-                    # Eat
-                    pass
+                    eaten = target.remove_health(self.eat_speed * DT)
+                    self.remove_hunger(eaten * Herbivore.HEALTH_TO_HUNGER)
+                    if self.hunger <= 0:
+                        self.state = Herbivore.Idle.random(0.5)
             case _:
                 raise ValueError("Unknown state")
         
+
+class Map(SpriteSolidColor):
+    scene = arcade.Scene()
+    def __init__(self) -> None:
+        super().__init__(800, 800, (0, 0, 0))
+        self.center_x = SCREEN_WIDTH / 2
+        self.center_y = SCREEN_HEIGHT / 2
+        for _ in range(30):
+            left = self.left + random.random() * (self.width - OBJ_SIZE)
+            top = self.top - random.random() * (self.height - OBJ_SIZE)
+            if random.random() < 0.4:
+                self.scene.add_sprite("herbivore", Herbivore(self, left, top))
+            else:
+                self.scene.add_sprite("grass", Grass(self, left, top))
+
+    def update(self):
+        for list in self.scene.sprite_lists:
+            for obj in list.sprite_list:
+                obj.update()
+                if obj.left < self.left:
+                    obj.change_x = abs(obj.change_x)
+                elif obj.right > self.right:
+                    obj.change_x = -abs(obj.change_x)
+                elif obj.top > self.top:
+                    obj.change_y = -abs(obj.change_y)
+                elif obj.bottom < self.bottom:
+                    obj.change_y = abs(obj.change_y)
+
+    def draw(self, **kwargs) -> None:
+        super().draw(**kwargs)
+        for obj in self.scene.sprite_lists:
+            for obj in obj.sprite_list:
+                obj.draw()
--- src/ep1/main.py
+++ src/ep1/main.py
@@ -1,38 +1,7 @@
 import arcade, arcade.color
-from arcade import Sprite, SpriteSolidColor, Window
-import random
-from constants import OBJ_SIZE, SCREEN_WIDTH, SCREEN_HEIGHT
-from agents import Herbivore, Grass
-
-
-class Map(SpriteSolidColor):
-    all_objs = arcade.SpriteList()
-    def __init__(self) -> None:
-        super().__init__(800, 800, (0, 0, 0))
-        self.center_x = SCREEN_WIDTH / 2
-        self.center_y = SCREEN_HEIGHT / 2
-        for _ in range(30):
-            left = self.left + random.random() * (self.width - OBJ_SIZE)
-            top = self.top - random.random() * (self.height - OBJ_SIZE)
-            if random.random() < 0.3:
-                self.all_objs.append(Herbivore(left, top))
-            else:
-                self.all_objs.append(Grass(left, top))
-    def update(self):
-        for obj in self.all_objs.sprite_list:
-            obj.update()
-            if obj.left < self.left:
-                obj.change_x = abs(obj.change_x)
-            elif obj.right > self.right:
-                obj.change_x = -abs(obj.change_x)
-            elif obj.top > self.top:
-                obj.change_y = -abs(obj.change_y)
-            elif obj.bottom < self.bottom:
-                obj.change_y = abs(obj.change_y)
-    def draw(self, **kwargs) -> None:
-        super().draw(**kwargs)
-        for obj in self.all_objs.sprite_list:
-            obj.draw()
+from arcade import Window
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from agents import Map
 
 class Game(Window):
     map = Map()
